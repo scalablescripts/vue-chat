e861c0c6cc2531381c39feb4e291b347e72a7500
Antonio Papa <antoniopapa1991@gmail.com>
2021-05-31 15:54:08 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+      <div class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+        <input class="fs-5 fw-semibold" v-model="username"/>
+      </div>
+      <div class="list-group list-group-flush border-bottom scrollarea">
+        <div class="list-group-item list-group-item-action py-3 lh-tight"
+             v-for="message in messages" :key="message"
+        >
+          <div class="d-flex w-100 align-items-center justify-content-between">
+            <strong class="mb-1">{{ message.username }}</strong>
+          </div>
+          <div class="col-10 mb-1 small">{{ message.message }}</div>
+        </div>
+      </div>
+    </div>
+    <form @submit.prevent="submit">
+      <input class="form-control" placeholder="Write a message" v-model="message"/>
+    </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref, onMounted} from 'vue';
+import Pusher from 'pusher-js';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const username = ref('username');
+    const messages = ref([]);
+    const message = ref('');
+
+    onMounted(() => {
+      Pusher.logToConsole = true;
+
+      const pusher = new Pusher('25291c0752d6089a660c', {
+        cluster: 'eu'
+      });
+
+      const channel = pusher.subscribe('chat');
+      channel.bind('message', data => {
+        messages.value.push(data);
+      });
+    })
+
+    const submit = async () => {
+      await fetch('http://localhost:8000/api/messages', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          username: username.value,
+          message: message.value
+        })
+      })
+
+      message.value = '';
+    }
+
+    return {
+      username,
+      messages,
+      message,
+      submit
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.scrollarea {
+  min-height: 500px;
 }
 </style>
